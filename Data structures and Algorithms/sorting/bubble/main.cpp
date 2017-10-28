a78204978a061493b1364a50e8add645cb2cf4fa
NaturalFlow <31296430+NaturalFlow@users.noreply.github.com>
2017-10-28 05:40:53 -0200

    Update main.cpp

--- Data structures and Algorithms/sorting/bubble/main.cpp
+++ Data structures and Algorithms/sorting/bubble/main.cpp
@@ -12,7 +12,7 @@
 int main() 
 {
     std::vector<int> data = {1,3,1,6,7,10,23,4};
-    bool isSorted = true
+    bool isSorted = true;
     while (isSorted) {
         isSorted = false;
         for (int j = 0; j < data.size() - 1; j++) {
